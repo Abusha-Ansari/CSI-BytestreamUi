cca1438411090658ccf7c2d5d805127f1752a1e4
Abusha Ansari <msaif8747@gmail.com>
2025-02-28 21:58:18 +0530

    Initialize Repo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen bg-amber-300 flex flex-row justify-center items-center p-3">
+      <h1 className="text-4xl text-center block font-bold">Bytestream</h1>
+    </div>
+  )
+}
+
+export default App
